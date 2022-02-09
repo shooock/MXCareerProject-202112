62a3499a39d1a420c3fb740532617d028adbbe2e
shooock <44541277+shooock@users.noreply.github.com>
2022-02-10 00:17:15 +0800

    Add files via upload
    链表还需要加强

---
+++ Homework/lecture5-6/复制带随机指针的链表.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head: return
+        cur = head
+        dic ={}
+        while cur:
+            dic[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        while cur:
+            dic[cur].next = dic.get(cur.next)
+            dic[cur].random = dic.get(cur.random)
+            cur = cur.next
+        return dic[head]
+
---
+++ Homework/lecture5-6/奇偶链表.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        odd = head
+        tem = even = head.next
+        while even and even.next:
+            odd.next = even.next
+            odd = even.next
+            even.next = odd.next
+            even = odd.next
+        odd.next = tem
+        return head
